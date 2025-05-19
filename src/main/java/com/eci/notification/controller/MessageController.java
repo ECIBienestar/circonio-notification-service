38998672ee70f92176d702d02c56d78fdb832633
Sebastian-villarraga <157900873+Sebastian-villarraga@users.noreply.github.com>
2025-05-19 14:38:30 -0500

    update pom

--- src/main/java/com/eci/notification/controller/MessageController.java
+++ src/main/java/com/eci/notification/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.eci.notification.controller;
 
+import com.eci.notification.model.NotificationMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MessageController {
 
-    @MessageMapping("/send") // cliente envía a /app/send
-    @SendTo("/topic/messages") // se retransmite a los subscriptores
-    public String broadcastMessage(String message) {
-        return message; // Aquí podrías mapear a un objeto si lo deseas
+    @MessageMapping("/send") 
+    @SendTo("/topic/messages") 
+    public NotificationMessage broadcastMessage(NotificationMessage message) {
+        return message;
     }
 }
