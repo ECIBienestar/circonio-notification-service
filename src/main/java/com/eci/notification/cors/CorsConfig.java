a6d61b6cedaf40debfac171b9fa134595fb12e79
Sebastian-villarraga <157900873+Sebastian-villarraga@users.noreply.github.com>
2025-05-23 14:10:50 -0500

    unit test2.1

--- src/main/java/com/eci/notification/cors/CorsConfig.java
+++ src/main/java/com/eci/notification/cors/CorsConfig.java
@@ -1,5 +1,23 @@
 package com.eci.notification.cors;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173") // Puerto de tu frontend con Vite
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
 }
